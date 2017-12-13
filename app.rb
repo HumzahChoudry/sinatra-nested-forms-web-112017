80a11c1a18413b9ab790d8b0e1f816c4507c363e
humzahchoudry <Humzah.choudry@gmail.com>
2017-12-12 19:33:27 -0500

    done:

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      #binding.pry
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+
+      @ships = params[:pirate][:ships].map do |ship|
+        Ship.new(ship[:name], ship[:type], ship[:booty])
+      end
+
+      erb :"pirates/show"
+    end
 
   end
 end
